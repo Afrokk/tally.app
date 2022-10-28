cf77eac1866577dbe678decaab4d85edd108241c
Afrokk <afrasiyabkhan379@gmail.com>
2022-10-28 00:15:11 -0400

    Add Server Files

---
+++ Server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const networkPort = process.env.networkPort || 3000;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ 
+    greeting: "You sent a GET request to the server",
+    message: "Hello from server!" });
+});
+
+app.listen(networkPort, () => {
+  console.log(`Server working on Port: ${networkPort}`);
+});
