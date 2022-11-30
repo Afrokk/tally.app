c7904e01a295404e2ded98f6fddd00ca8a5ddc93
Afrokk <afrasiyabkhan379@gmail.com>
2022-11-30 00:40:47 -0500

    Fix span

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
   return (
     <div className='header'>
       <h1 className="logo">tally<span>.app</span></h1>
-      <h3 className="tagline">An interactive <span>Score</span> Keeper</h3>
+      <h3 className="tagline">An interactive <span>Score Keeper</span></h3>
     </div>
   );
 };
